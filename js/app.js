ce0209160bcec62e07542e0377cdfc7490c074fc
Shubh <Shubhammiyani2006@gmail.com>
2026-03-06 11:43:00 +0530

    first

---
+++ js/app.js
@@ -0,0 +1,69 @@
+// setTimeout(() => {
+//     console.log("one.one");
+// }, 2000);
+// console.log("two")
+// setTimeout(() => {
+//     console.log("rwo.two");
+// }, 0);
+
+// let a = true;
+// setTimeout(() => {
+//     let count=0;
+//     while(a)
+//     {
+//         count++;
+//         console.log("count");
+//     }
+// }, 0)
+// a=!a;
+
+// function sayhello(){
+//     console.log("hello")
+// }
+
+// sayhello();
+
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(2,4));
+// console.log(add('a','b'));
+
+// function product(a,b){
+//     return a*b;
+// }
+// console.log(product(2,6));
+// console.log(product(12,45));
+// console.log(product(24,36));
+
+// function product(a,b){
+//     return a*b;
+// }
+// let product1=(a,b)=>a*b;
+
+// console.log(product1(2,6));
+// console.log(product1(12,45));
+// console.log(product1(24,36));
+
+// function a(d){
+//     d();
+//     console.log("inside a");
+// }
+// function b(){
+//     console.log("inside b");
+// }
+// a(b);
+
+// function outer(){
+//     let count=0;
+//     function inner(){
+//         count++;
+//         console.log(count);
+//     }
+//     return inner;
+// }
+// let res = outer();
+// res();
+// res();
+
+
